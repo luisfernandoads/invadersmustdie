62e3ce4cd02248d0305729c67a85a16970ea8cb5
Wick <wickerdj@gmail.com>
2015-08-05 23:25:34 -0500

    Completed part 1

--- project/conf.lua
+++ project/conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
 	t.version = "0.9.1"
 	t.window.title = "Invaders Must Die"
-	t.window.width = 600
-	t.window.height = 400
+	t.window.width = 800
+	t.window.height = 600
+
+	t.console = true
 end
--- project/main.lua
+++ project/main.lua
@@ -1,11 +1,22 @@
 function love.load()
-
+	hero = {}
+	hero.x = 300
+ 	hero.y = 450
+	hero.speed = 100	
 end
 
 function love.update(dt)
-
+	if love.keyboard.isDown("left") then
+		hero.x = hero.x - hero.speed*dt
+	elseif love.keyboard.isDown("right") then
+		hero.x = hero.x + hero.speed*dt
+	end
 end
 
 function love.draw()
+	love.graphics.setColor(0,255,0,255)
+	love.graphics.rectangle("fill", 0, 465, 800, 150)
 
+	love.graphics.setColor(255,255,0,255)
+	love.graphics.rectangle("fill", hero.x, hero.y, 30, 15)
 end
